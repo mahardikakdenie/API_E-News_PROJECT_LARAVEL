1fea91da8244f95fb8ee21b92ac517a38ff04d44
mahardika22 <dikamahar884@gmail.com>
2021-03-19 22:19:26 +0700

    updates

--- app/Thumbnail.php
+++ app/Thumbnail.php
@@ -13,7 +13,7 @@ class Thumbnail extends Model
     public function getUrlAttribute()
     {
         if ($this->attributes['url'] != null) {
-            return  env('IMAGE_URL', 'http://127.0.0.1:8000/storage/') . $this->attributes['url'];
+            return  env('IMAGE_URL', 'https://mynews22.herokuapp.com/storage/') . $this->attributes['url'];
         } else {
             return $this->attributes['url'];
         }
